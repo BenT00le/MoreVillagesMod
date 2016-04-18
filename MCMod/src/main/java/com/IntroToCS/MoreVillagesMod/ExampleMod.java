c35eaef3b79925e92c40facd21129d2ac6bd1c57
BenT00le <ben4toole@gmail.com>
2016-04-17 20:27:48 -0700

    Comments and Crafting recipe added
    a crafting recipe was added for the item and some comments where added
    to explain the recipe and other parts of the code

--- MCMod/src/main/java/com/IntroToCS/MoreVillagesMod/ExampleMod.java
+++ MCMod/src/main/java/com/IntroToCS/MoreVillagesMod/ExampleMod.java
@@ -1,40 +1,43 @@
-package com.example.examplemod;
+package com.IntroToCS.MoreVillagesMod;
 
+import com.IntroToCS.MoreVillagesMod.ItemOne;
+import com.IntroToCS.MoreVillagesMod.References;
 import Proxy.CommonProxy;
-import com.example.examplemod.ItemOne;
-import com.example.examplemod.References;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;;
 
 @Mod(modid = References.MODID, name = References.ModName, version = References.VERSION)
 public class ExampleMod
 {
 	@SidedProxy(clientSide = References.ClientProxyClass,serverSide = References.ServerProxyClass) 
 	public static CommonProxy proxy;
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
-    
+	
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-    	ItemOne.init();;
-    	ItemOne.Register();;
+    	ItemOne.init();
+    	ItemOne.Register();
     }
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-    	proxy.regesterRenders();;
+    	//creates a crafting recipe for "ItemOne" which will become a prismarine helmet
+    	GameRegistry.addRecipe(new ItemStack(ItemOne.Item_One), new Object[]{"PPP",
+    			                                                             "P P", //each set of quotes is one row in a crafting table spaces represent empty slots
+    			                                                             "P P",'P',Items.prismarine_shard});
+    	proxy.regesterRenders();
     }
     @EventHandler
     public void postInit(FMLPostInitializationEvent event)
     {
-
-		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    	
     }
 }
