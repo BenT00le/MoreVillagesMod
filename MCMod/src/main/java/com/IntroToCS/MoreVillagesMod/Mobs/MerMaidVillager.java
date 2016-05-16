2ba2690dce19a1e583712675f2db01a82e9eea3f
BenT00le <ben4toole@gmail.com>
2016-05-15 19:23:31 -0700

    mermaid in right file location
    moved mermaidvillager file location

--- MCMod/src/main/java/com/IntroToCS/MoreVillagesMod/Mobs/MerMaidVillager.java
+++ MCMod/src/main/java/com/IntroToCS/MoreVillagesMod/Mobs/MerMaidVillager.java
@@ -1,13 +1,40 @@
 package com.IntroToCS.MoreVillagesMod.Mobs;
 
+import net.minecraft.entity.ai.EntityAIPanic;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAIWander;
 import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.world.World;
 
 public class MerMaidVillager extends EntityVillager 
 {
-	public MerMaidVillager(World worldIn) {
+	public MerMaidVillager(World worldIn) 
+	{
 		super(worldIn);
-		
+		this.setSize(1.0F,2.0F);
+		this.tasks.addTask(0, new EntityAISwimming(this));
+		this.tasks.addTask(1, new EntityAIPanic(this,2.0D));
+		this.tasks.addTask(2, new EntityAIWander(this,1.3D));
+	}
+	public boolean isPushedByWater()
+	{
+		return false;
+	}
+	public void onLivingUpdate()
+	{
+		super.onLivingUpdate();
+		this.setAir(300);
+		this.limbSwing = 0;
+		this.limbSwingAmount = 0;
+		this.prevLimbSwingAmount = this.limbSwing;
+		if(!this.isInWater())
+		{
+			this.kill();
+		}
+	}
+	public boolean canBreathUnderwater()
+	{
+		return true;
 	}
 
 }
